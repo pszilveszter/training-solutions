05f758e26ac28df1c74513bd84c100cff56177cb
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-01 19:24:46 +0100

    Registration: Set UILang = hu

--- src/main/java/classstructureio/Registration.java
+++ src/main/java/classstructureio/Registration.java
@@ -6,15 +6,15 @@ public class Registration {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("This class gains name and email information from user");
-        System.out.println("Please enter your name");
+        System.out.println("Ez az osztály név és email adatokat kér be a felhasználótól!");
+        System.out.println("Add meg a neved:");
 
         String name = scanner.nextLine();
-        System.out.println("Provide email address too");
+        System.out.println("Add meg az email címed:");
         String email = scanner.nextLine();
 
-        System.out.println("Registration requested with the following data:");
-        System.out.println("\tName: " + name);
+        System.out.println("Regisztráció az alábbi adatokkal:");
+        System.out.println("\tNév: " + name);
         System.out.println("\tEmail: " + email);
 
     }
