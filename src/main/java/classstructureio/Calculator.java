ea4e1c9727f9db34646836ee6a00ae9d65476070
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-01 19:20:38 +0100

    Set UILang = hu

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("This class provides the summary of two integers that are based on user input!");
-        System.out.println("Please enter the first number");
+        System.out.println("Ez az osztály a felhasználó által bevitt két egész szám összegét számolja ki!");
+        System.out.println("Kérlek írd be az első számot:");
         Scanner scanner = new Scanner(System.in);
 
         int numA = scanner.nextInt();
-        System.out.println("Enter the second number");
+        System.out.println("A második szám:");
         int numB = scanner.nextInt();
 
         System.out.println(numA + " + " + numB);
-        System.out.println(numA+numB);
+        System.out.println(numA + numB);
     }
 }
