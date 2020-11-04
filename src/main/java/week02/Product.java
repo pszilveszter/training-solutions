2f7bddc9af1c00443f691fb6bcd978c131e0574d
Szilveszter Pinter <szilveszterp.pinter@betssongroup.com>
2020-11-04 13:13:21 +0100

    20201104 feladat

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,34 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product p) {
+        return name.equals(p.name);
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("név","code1");
+        Product p2 = new Product("név","code2");
+        Product p3 = new Product("név3","code3");
+
+        System.out.println(p1.areTheyEqual(p2));
+        System.out.println(p1.areTheyEqual(p3));
+
+        // kódok különbsége???? MA nem tudok beszélni, de ezt a részt nem értem
+    }
+}
