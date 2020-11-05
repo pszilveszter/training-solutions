6425645d0eb3b611aa56888f5c4f7877c1388951
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-05 21:21:38 +0100

    Introcontrol v1.0

---
+++ src/main/java/introcontrol/IntroControl.java
@@ -0,0 +1,79 @@
+package introcontrol;
+
+public class IntroControl {
+    public int substractTenIfGreaterThanTen(int number) {
+        int x = 10;
+        boolean b = !(number > 10);
+
+        if (b) {
+            return number;
+        }
+        return  number - x;
+    }
+
+    public String describeNumber(int number) {
+        if (number == 0) {
+            return "zero";
+        }
+        return "not zero";
+    }
+
+    public String greetingToJoe(String name) {
+        if (name.equals("Joe")) {
+            return "Hello Joe";
+        }
+        return "";
+    }
+
+    public int calculateBonus(int sale) {
+        int min = 1_000_000;
+        if (!(sale < min)) {
+            sale = sale / 100 * 10;
+        } else {
+            sale = 0;
+        }
+        return sale;
+    }
+
+    public int calculateConsumption(int prev, int next) {
+        if (next == 9999) next = 0;
+        return Math.abs(next - prev);
+    }
+
+    public void printNumbers(int max) {
+        for (int i = 0; i <= max; i++) {
+            System.out.print(i + ";");
+        }
+        System.out.println();
+    }
+
+    public void printNumbersBetween(int min, int max) {
+            for (;min <= max; min++) System.out.print(min + ";");
+        System.out.println();
+    }
+
+    public void printNumbersBetweenAnyDirection(int a, int b) {
+        if (a < b) {
+            for (;a <= b; a++) {
+                System.out.print(a + ";");
+            }
+        } else {
+            for (;a >= b; a--) {
+                System.out.print(a + ";");
+            }
+        }
+        System.out.println();
+    }
+
+    public void printOddNumbers(int max) {
+        if (max % 2 == 1) {
+            System.out.println("páratlan");
+            for (int i = 1; i <= max; i = i + 2) {
+                System.out.print(i + ";");
+            }
+            System.out.println();
+        } else {
+            System.out.println("páros");
+        }
+    }
+}
---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,40 @@
+package introcontrol;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl control = new IntroControl();
+
+        int x = 11;
+        System.out.println(control.substractTenIfGreaterThanTen(x));
+
+        int y = 0;
+        System.out.println(control.describeNumber(x));
+        System.out.println(control.describeNumber(y));
+
+        String nameA = "Donald";
+        String nameB = "Joe";
+        System.out.println(control.greetingToJoe(nameA));
+        System.out.println(control.greetingToJoe(nameB));
+
+        int q1 = 999_999;
+        int q2 = 1_000_000;
+        System.out.println(control.calculateBonus(q1));
+        System.out.println(control.calculateBonus(q2));
+
+        int prev1 = 1;
+        int next1 = 9998;
+        int prev2 = 1;
+        int next2 = 9999;
+        System.out.println(control.calculateConsumption(prev1,next1));
+        System.out.println(control.calculateConsumption(prev2,next2));
+
+        control.printNumbers(x);
+        control.printNumbersBetween(prev1, x);
+
+        control.printNumbersBetweenAnyDirection(prev1,x);
+        control.printNumbersBetweenAnyDirection(x, prev1);
+
+        control.printOddNumbers(x);
+        control.printOddNumbers(next1);
+    }
+}
