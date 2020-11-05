72d38741a419c70d938aca1267b8cde02551c71d
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-05 06:12:03 +0100

    Upper v1.0

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,11 @@
+package javaapi;
+
+import javax.swing.*;
+
+public class Upper {
+    public static void main(String[] args) {
+        String message = "Hello World!";
+        String capitalMessage = message.toUpperCase();
+        System.out.println(capitalMessage);
+    }
+}
