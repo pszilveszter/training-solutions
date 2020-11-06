a70f7587438c5f2525f707c79cb7c416404aafcf
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-06 13:17:17 +0100

    Languages v1.0

--- src/main/java/week02/Languages.java
+++ src/main/java/week02/Languages.java
@@ -1,9 +1,17 @@
 package week02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Languages {
     public static void main(String[] args) {
-        String[] languages = {"Java", "Pyton", "JavaScript"};
-        for (String item: languages) {
+        List<String> listA = new ArrayList<>();
+
+        listA.add("Java");
+        listA.add("Pyton");
+        listA.add("JavaScript");
+
+        for (String item: listA) {
             if (item.length() > 5) System.out.println(item);
         }
     }
