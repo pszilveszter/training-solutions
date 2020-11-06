2895f9fa8a1e2854f89eec80f20747775a298c5e
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-06 16:28:50 +0100

    triangularMatrix v1.0

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -1,6 +1,25 @@
 package arrayofarrays;
 
 public class ArrayOfArraysMain {
+    public static void main(String[] args) {
+        ArrayOfArraysMain array = new ArrayOfArraysMain();
+        int[][] multiplicationResult = array.multiplicationTable(4);
+        array.printArrayofArrays(multiplicationResult);
+        int[][] triangle = array.tringularMatrix(16);
+        array.printArrayofArrays(triangle);
+    }
+
+    public int[][] tringularMatrix(int size) {
+        int[][] triangle = new int[size][];
+        for (int i = 0; i < size; i++) {
+            triangle[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = i + 1;
+            }
+        }
+        return triangle;
+    }
+
     public int[][] multiplicationTable(int size) {
         int[][] table = new int[size][size];
         for (int i = 0; i < size; i++) {
@@ -20,9 +39,5 @@ public class ArrayOfArraysMain {
             System.out.println(line);
         }
     }
-    public static void main(String[] args) {
-        ArrayOfArraysMain array = new ArrayOfArraysMain();
-        int[][] multiplicationResult = array.multiplicationTable(4);
-        array.printArrayofArrays(multiplicationResult);
-    }
+
 }
