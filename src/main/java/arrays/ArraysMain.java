8db804aa91fd1c9076304ece03b573453020718f
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-06 19:35:05 +0100

    daysOfWeek v1.0

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArraysMain {
@@ -9,13 +10,29 @@ public class ArraysMain {
 
         System.out.println("numberOfDaysAsString");
         System.out.println(array.numberOfDaysAsString());
+
+        System.out.println("daysOfWeek");
+        System.out.println(array.daysOfWeek());
     }
 
-/* Ajaj, ez a feladat nekem listának tünik, amit még nem vettem, visszatérek később
+/*************************************************************************************
+* Ajaj, ez a feladat nekem listának tünik, amit még nem vettem, visszatérek később
+* UPDATE: Nem baj, azért megpróbálom a korábbi tapasztalatok alapján
+* Hát csak sikerült, remélem jól értelmeztem
+************************************************************************************** */
     public List<String> daysOfWeek() {
+        List<String> dayNames= new ArrayList<>();
+        dayNames.add("Hétfő");
+        dayNames.add("Kedd");
+        dayNames.add("Szerda");
+        dayNames.add("Csütörtök");
+        dayNames.add("Péntek");
+        dayNames.add("Szombat");
+        dayNames.add("Vasárnap");
 
+        return dayNames;
     }
-*/
+
 
     public String numberOfDaysAsString() {
         int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
