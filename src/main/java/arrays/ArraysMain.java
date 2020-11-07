b490dff9aeb3dc29bd16bd72e79c4112cd67bfb7
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-07 10:27:01 +0100

    wonLottery v1.0

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -24,6 +24,40 @@ public class ArraysMain {
         System.out.println(array.sameTempValues(day1, day2));
         System.out.println(array.sameTempValues(day2, day3));
 
+        System.out.println("\nwonLottery"); //renzedési vizsgálat behúzással
+        int[] bet = {88, 77, 4, 13, 23};
+        int[] drawn = {23, 4, 88, 13, 77};
+            int betSize = bet.length;
+            int drawnSize = drawn.length;
+            int[] betArchive = Arrays.copyOf(bet,betSize);
+            int[] drawnArchive = Arrays.copyOf(drawn,drawnSize);
+
+        System.out.println("Eredmény: " + array.wonLottery(bet, drawn));
+            if (Arrays.equals(bet, betArchive) && Arrays.equals(drawn, drawnArchive)) {
+                System.out.println("--- A rendezésnek immáron nincs visszahatása ---");
+            } else {
+                System.out.println("XXX A rendezésnek van visszahatása XXX");
+            }
+    }
+
+
+    public boolean wonLottery(int[] betNumbers, int[] drawnNumbers) {
+            int betSize = betNumbers.length;
+            int drawnSize = drawnNumbers.length;
+            int[] bet = Arrays.copyOf(betNumbers, betSize);
+            int[] drawn = Arrays.copyOf(drawnNumbers, drawnSize);
+
+        /*
+        ERDEETI
+        Arrays.sort(betNumbers);
+        Arrays.sort(drawnNumbers);
+        return Arrays.equals(betNumbers, drawnNumbers);
+
+         */
+
+            Arrays.sort(bet);
+            Arrays.sort(drawn);
+            return Arrays.equals(bet, drawn);
     }
 
 
