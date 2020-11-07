4d5462e098b9fce7eadf70ec0bb4465be00e6b21
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-07 21:51:48 +0100

    Bonyolultabb tipusok / Debug / Alkalmazottak v1.0

---
+++ src/main/java/debug/CompanyMain.java
@@ -0,0 +1,29 @@
+package debug;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyMain {
+    public static void main(String[] args) {
+        Employee rambo = new Employee("John Rambo", 1982);
+        Employee rocky = new Employee("Rocky Balboa", 1976);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(rambo);
+        employees.add(rocky);
+
+        Company company = new Company(employees);
+
+        Employee tango = new Employee("Ray Tango", 1989);
+        Employee sly = new Employee("Extra Elek", 2020);
+        company.addEmployee(tango);
+
+        Employee ray = company.findEmployeeByName("Ray Tango");
+        System.out.println(ray.getName() + " - (" + ray.getYearOfBirth() + ")");
+
+        List<String> allEmployeesOfCompany = new ArrayList<>();
+        allEmployeesOfCompany.addAll(company.listEmpoyeeNames());
+        System.out.println(allEmployeesOfCompany);
+
+    }
+}
