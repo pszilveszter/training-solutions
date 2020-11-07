a5fa8467b474740019cf19f57d2bc59a10e72067
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-07 10:50:55 +0100

    Arrays v2.0 - code readibility to ease review (not necessary based on conventions)

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -9,47 +9,47 @@ public class ArraysMain {
         ArraysMain array = new ArraysMain();
 
         System.out.println("numberOfDaysAsString");
-        System.out.println(array.numberOfDaysAsString());
+            System.out.println(array.numberOfDaysAsString());
 
         System.out.println("\ndaysOfWeek");
-        System.out.println(array.daysOfWeek());
+            System.out.println(array.daysOfWeek());
 
         System.out.println("\nmultiplicationTableAsString");
-        System.out.println(array.multiplicationTableAsString(5));
+            System.out.println(array.multiplicationTableAsString(5));
 
         System.out.println("\nsameTempValues");
-        int whole = 20;
-        double[] day1 = new double[24];
-        double[] day2 = new double[24];
-        double[] day3;
-        for (int i = 0; i < day1.length; i++) {
-            double decimal = i * 0.1;
-            day1[i] = whole + decimal;
-            if (i == 1) {
-                day2[i] = whole + 2 * decimal;
-            } else {
-                day2[i] = whole + decimal;
+            int whole = 20;
+            double[] day1 = new double[24];
+            double[] day2 = new double[24];
+            double[] day3;
+            for (int i = 0; i < day1.length; i++) {
+                double decimal = i * 0.1;
+                day1[i] = whole + decimal;
+                if (i == 1) {
+                    day2[i] = whole + 2 * decimal;
+                } else {
+                    day2[i] = whole + decimal;
+                }
             }
-        }
-        day3 = Arrays.copyOf(day1, 24);
+            day3 = Arrays.copyOf(day1, 24);
 
-        System.out.println(array.sameTempValues(day1, day2));
-        System.out.println(array.sameTempValues(day1, day3));
+            System.out.println(array.sameTempValues(day1, day2));
+            System.out.println(array.sameTempValues(day1, day3));
 
-        System.out.println("\nwonLottery"); //renzedési vizsgálat behúzással
-        int[] bet = {88, 77, 4, 13, 23};
-        int[] drawn = {23, 4, 88, 13, 77};
-            int betSize = bet.length;
-            int drawnSize = drawn.length;
-            int[] betArchive = Arrays.copyOf(bet,betSize);
-            int[] drawnArchive = Arrays.copyOf(drawn,drawnSize);
+        System.out.println("\nwonLottery");
+            int[] bet = {88, 77, 4, 13, 23};
+            int[] drawn = {23, 4, 88, 13, 77};
+                int betSize = bet.length; //renzedési vizsgálat behúzással
+                int drawnSize = drawn.length;
+                int[] betArchive = Arrays.copyOf(bet,betSize);
+                int[] drawnArchive = Arrays.copyOf(drawn,drawnSize);
 
-        System.out.println("Eredmény: " + array.wonLottery(bet, drawn));
-            if (Arrays.equals(bet, betArchive) && Arrays.equals(drawn, drawnArchive)) {
-                System.out.println("--- A rendezésnek immáron nincs visszahatása ---");
-            } else {
-                System.out.println("XXX A rendezésnek van visszahatása XXX");
-            }
+            System.out.println("Eredmény: " + array.wonLottery(bet, drawn));
+                if (Arrays.equals(bet, betArchive) && Arrays.equals(drawn, drawnArchive)) {
+                    System.out.println("--- A rendezésnek immáron nincs visszahatása ---");
+                } else {
+                    System.out.println("XXX A rendezésnek van visszahatása XXX");
+                }
     }
 
 
