21bb703f9048064b227c4d4aacb061a87b1ddb51
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-09 08:34:30 +0100

    Bonyolultabb tipusok / finalmodifier / Gentleman v1.1 - sayHello added

--- src/main/java/finalmodifier/Gentleman.java
+++ src/main/java/finalmodifier/Gentleman.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class Gentleman {
     public static final String MESSAGE_PREFIX = "Hello";
 
+
     public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        String name = new Gentleman().askName();
+        gentleman.sayHello(name);
+    }
+
+    public void sayHello(String name) {
+        System.out.println(MESSAGE_PREFIX + " " + name);
+    }
+
+    public String askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(MESSAGE_PREFIX + "! Kérlek add meg a neved:");
-        String name = scanner.nextLine();
-        System.out.println(MESSAGE_PREFIX + " " + name);
+        return scanner.nextLine();
     }
 }
