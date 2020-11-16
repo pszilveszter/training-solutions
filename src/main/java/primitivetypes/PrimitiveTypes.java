3f498b3d7c4155e0ee4946ba0d867465312e3ab7
Szilveszter Pinter <szilveszter.pipnter@betssongroup.com>
2020-11-16 21:15:07 +0100

    literals.LiteralsMain v1.0

--- src/main/java/primitivetypes/PrimitiveTypes.java
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -2,7 +2,9 @@ package primitivetypes;
 
 public class PrimitiveTypes {
     private final static int BITS = 32;
+
     public static String toBinaryString(int n) {
+        // FELADAT: adott pozitív egész számot kettes számrendszerbe váltja át
         String result = "";
         for (int i = 0; i < BITS; i++) {
             if (n > 0) {
