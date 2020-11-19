8e4a21eca662c96c9d9c9c573d9b4b3e433d6930
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-19 10:51:18 +0100

    week03.Positions v1.0

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,42 @@
+package week03;
+
+import java.io.PipedOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public static void main(String[] args) {
+
+        List<Position> postitions = new ArrayList<>();
+        postitions.add(new Position("Főnök",1_000_000));
+        postitions.add(new Position("Vezető", 500_000));
+        postitions.add(new Position("Beosztott",100_000));
+
+        for (Position p : postitions) {
+            if (p.getBonus() > 150_000) {
+                System.out.println(p.toString());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + bonus;
+    }
+}
