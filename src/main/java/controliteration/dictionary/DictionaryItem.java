47622d148296b690b50a7f5932e159a2674cedae
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-21 17:31:22 +0100

    controliteration.dictionary.bonus v1.1

--- src/main/java/controliteration/dictionary/DictionaryItem.java
+++ src/main/java/controliteration/dictionary/DictionaryItem.java
@@ -12,6 +12,9 @@ public class DictionaryItem {
     public DictionaryItem(String word, List<String> translations) {
         this.word = word;
         addTranslations(translations);
+        // Bonusz: ha a 2-ik paramétert egy az egyben adnánk értékül,
+        // akkor az addTranslation metódus nem fut le
+        // és ezért létre lehet hozni egy fordítást akár többször is a konstruktorban
     }
 
     public void addTranslations(List<String> newTranslations) {
