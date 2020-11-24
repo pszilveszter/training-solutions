8afd7dd87d2b0a2285b438700d321666bc65a98d
pszilveszter <szilveszter.pinter@yahoo.com>
2020-11-24 13:19:18 +0100

    week05d02 v1.1

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -6,7 +6,7 @@ public class ChangeLetter {
     public String changeVowels(String input) {
         String result = "";
         for (int i = 0; i < input.length(); i++) {
-            if (ENGVOW.indexOf(input.charAt(i)) > -1 ) {
+            if (ENGVOW.indexOf(input.charAt(i)) <= -1 ) {
                 result += input.charAt(i);
             } else {
                 result += "*";
