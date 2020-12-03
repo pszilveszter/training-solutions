63c7ff2868a84c9c1ca806d236bfbe5021dfb645
pszilveszter <szilveszter.pinter@yahoo.com>
2020-12-03 09:58:49 +0100

    week06d03 v1.1 - complete testing with repetitive case

--- src/test/java/week06d03/WordEraserTest.java
+++ src/test/java/week06d03/WordEraserTest.java
@@ -10,25 +10,25 @@ class WordEraserTest {
 
     @Test
     void testWordEraserNotAround() {
-        String result = we.eraseWord("banán körte alma barack alma banán", "alma");
+        String result = we.eraseWord("banán körte alma alma barack alma banán", "alma");
         assertEquals("banán körte barack banán", result);
     }
 
     @Test
     void testWordEraserHavingTargetAround() {
-        String result = we.eraseWord("alma körte alma barack alma", "alma");
+        String result = we.eraseWord("alma körte alma alma barack alma", "alma");
         assertEquals("körte barack", result);
     }
 
     @Test
     void testEnhancedNotAround() {
-        String result = we.eraseWordEnhanced("banán körte alma barack alma banán", "alma");
+        String result = we.eraseWordEnhanced("banán körte alma alma barack alma banán", "alma");
         assertEquals("banán körte barack banán", result);
     }
 
     @Test
     void testEnhancedHavingTargetAround() {
-        String result = we.eraseWordEnhanced("alma körte alma barack alma", "alma");
+        String result = we.eraseWordEnhanced("alma körte alma alma barack alma", "alma");
         assertEquals("körte barack", result);
     }
 }
