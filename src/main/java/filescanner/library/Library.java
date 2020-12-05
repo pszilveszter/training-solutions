bde412a642689f22ff40fb3a098132bd2a1e8c43
pszilveszter <szilveszter.pinter@yahoo.com>
2020-12-05 13:13:39 +0100

    filescanner.bucketlist v1.0

---
+++ src/main/java/filescanner/library/Library.java
@@ -0,0 +1,23 @@
+package filescanner.library;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public void loadFromFile() {
+        try (Scanner rb = new Scanner(Library.class.getResourceAsStream("/books.csv"))) {
+            rb.useDelimiter(";|(\r\n)");
+            while (rb.hasNextLine()) {
+                Book book = new Book(rb.next(), rb.next(), rb.next(), rb.nextInt());
+                books.add(book);
+            }
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
