6af58af3cdb06dd52175fbe535ebe976655cbd9e
pszilveszter <szilveszter.pinter@yahoo.com>
2020-12-15 15:59:12 +0100

    week08d01 v1.0

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -9,12 +9,10 @@ public class Lottery {
     public List<Integer> getNumbers(int interval, int type) {
         List<Integer> numbers = new ArrayList<>();
         Random rnd = new Random(100);
-        int c = 0;
-        while (c < type) {
+        while (numbers.size() < type) {
             int x = rnd.nextInt(interval) + 1;
             if (!numbers.contains(x)) {
                 numbers.add(x);
-                c++;
             }
 
         }
