2c0f1cb283b615c6047ab273da8c610473565540
pszilveszter <szilveszter.pinter@yahoo.com>
2020-12-16 15:38:11 +0100

    week08d03 v1.0

---
+++ src/main/java/week08d03/StringLists.java
@@ -0,0 +1,23 @@
+package week08d03;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StringLists {
+
+    public List<String> stringListsUnion(List<String> first, List<String> second) {
+        List<String> result = new ArrayList<>();
+        for (String s: first) {
+            if (!result.contains(s)) {
+                result.add(s);
+            }
+        }
+        for (String s: second) {
+            if (!result.contains(s)) {
+                result.add(s);
+            }
+        }
+    return result;
+    }
+}
---
+++ src/test/java/week08d03/StringListsTest.java
@@ -0,0 +1,23 @@
+package week08d03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringListsTest {
+
+    List<String> a = Arrays.asList("alma","körte", "alma");
+    List<String> b = Arrays.asList("barack","körte", "szilva");
+    List<String> expected = Arrays.asList("alma", "körte", "barack", "szilva");
+
+    @Test
+    void testStringLists() {
+        List<String> result = new StringLists().stringListsUnion(a,b);
+        assertEquals(expected.toString(), result.toString());
+    }
+
+
+}
