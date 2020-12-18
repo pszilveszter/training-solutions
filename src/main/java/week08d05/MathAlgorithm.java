c677b1724307d92826bd5d72f5df7ced145855cd
pszilveszter <szilveszter.pinter@yahoo.com>
2020-12-18 16:10:34 +0100

    week08d05 v1.0

--- src/main/java/week08d05/MathAlgorithm.java
+++ src/main/java/week08d05/MathAlgorithm.java
@@ -3,7 +3,7 @@ package week08d05;
 public class MathAlgorithm {
 
     public int greatestCommonDivisor(int a, int b) {
-        int o = b;
+        int o = Math.min(a,b);
         while (a % o != 0 || b % o != 0) {
             o--;
         }
