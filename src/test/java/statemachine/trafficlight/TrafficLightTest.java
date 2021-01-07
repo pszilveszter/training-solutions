4a3f7c4760af23f422bb8604248f0da122e98cb7
pszilveszter <szilveszter.pinter@yahoo.com>
2021-01-07 07:24:06 +0100

    statemachine.trafficlight v1.0

---
+++ src/test/java/statemachine/trafficlight/TrafficLightTest.java
@@ -0,0 +1,17 @@
+package statemachine.trafficlight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrafficLightTest {
+
+    @Test
+    public void testNext() {
+        assertEquals(TrafficLight.RED_YELLOW, TrafficLight.RED.next());
+        assertEquals(TrafficLight.GREEN, TrafficLight.RED_YELLOW.next());
+        assertEquals(TrafficLight.YELLOW, TrafficLight.GREEN.next());
+        assertEquals(TrafficLight.RED, TrafficLight.YELLOW.next());
+    }
+
+}
