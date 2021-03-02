b63e05e6389388511e18101f6fc29bd59adb58df
pszilveszter <szilveszter.pinter@yahoo.com>
2021-03-02 12:53:06 +0100

    genericsusage.withgenerics fix: NullPointerException v1

--- src/main/java/genericsusage/withgenerics/Library.java
+++ src/main/java/genericsusage/withgenerics/Library.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class Library {
 
     public Book getFirstBook(List<Book> books) {
-        if (books == null || books.isEmpty()) {
+        if (books == null) {
+            throw new NullPointerException("Argument should not be null!");
+        }
+        if (books.isEmpty()) {
             throw new IllegalArgumentException("Argument should not be empty!");
         }
         return books.get(0);
