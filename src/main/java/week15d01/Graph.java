c0cea62eef7f04c4287e9e1c43f5327095ce7893
pszilveszter <szilveszter.pinter@yahoo.com>
2021-03-10 14:42:08 +0100

    week15d01

---
+++ src/main/java/week15d01/Graph.java
@@ -0,0 +1,21 @@
+package week15d01;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class Graph {
+
+    public Map.Entry<Integer, Integer> getMaxPoint(Map<Integer, Integer> coordinates) {
+        if (coordinates.isEmpty()) {
+            throw new IllegalArgumentException("Empty input");
+        }
+
+        Map.Entry<Integer, Integer> result = null;
+        for (Map.Entry<Integer, Integer> entry: coordinates.entrySet()) {
+            if (result == null || result.getValue() < entry.getValue()) {
+                result = entry;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/week15d01/GraphTest.java
@@ -0,0 +1,30 @@
+package week15d01;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+
+    @Test
+    void testGraph() {
+        Map<Integer, Integer> points = new HashMap();
+        points.put(-4, -6);
+        points.put(-9, -11);
+        points.put(-1, 7);
+        points.put(3, 8);
+        points.put(3, -2);
+        points.put(1, 15);
+
+        Graph input = new Graph();
+        assertEquals("1=15", input.getMaxPoint(points).toString());
+
+    }
+
+}
